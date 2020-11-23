ace5869400740fd39da4f477973b37de2bc6c841
otpidusprime <sudiptomondal@pm.me>
2020-11-23 21:00:22 +0530

    Convert to functional component & use hooks
    Convert class component to functional, use hooks

--- src/App.js
+++ src/App.js
@@ -1,61 +1,71 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import TodoItem from "./components/TodoItem"
 import todoData from "./todoData"
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      todos: todoData
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
+function App() {
 
-  handleClick(id) {
-    this.setState((prevState) => {
+  const [todos, setTodos] = useState(todoData)
 
-      const updateTodo = prevState.todos.map(todo => {
-        if(todo.id === id) {
-          return {
-          ...todo,
-          completed: !todo.completed
-          }
-        }
-        return todo
-      })
+  // useEffect(() => {
+  //   if("local-todo" in localStorage) {
+  //     const LOCAL_TODO = JSON.parse(localStorage.getItem("local-todo"))
+  //     setTodos(LOCAL_TODO)
+  //   }
+  // },[])
 
-      return {
-        todos: updateTodo
-      }
+  useEffect(() => {
+    const LOCAL_TODO = localStorage.getItem("local-todo")
+    LOCAL_TODO && setTodos(JSON.parse(LOCAL_TODO))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("local-todo", JSON.stringify(todos))
+  })
+
+  const handleClick = (id) => {
+    setTodos(prevTodos => {
+      const updateTodo = prevTodos.map(todo => {
+            if(todo.id === id) {
+              return {
+                ...todo,
+                completed: !todo.completed
+              }
+            }
+          return todo
+        })
+        return updateTodo
     })
   }
 
-  componentDidMount() {
-    if(localStorage.getItem("local-todo")){
-      this.setState({
-        todos: JSON.parse(localStorage.getItem("local-todo"))
-      })
-    }
-  }
+  return (
+    <div className="todo-list">
+        {todos.map(item => <TodoItem key={item.id} item={item} handleClick={handleClick}/>)}
+    </div>
+  )
 
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.todos !== this.state.todos) {
-      this.setState({
-        localStorage: localStorage.setItem("local-todo", JSON.stringify(this.state.todos))
-      })
-    }
-  }
-  
-  render() {
-
-    const todoList = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleClick={this.handleClick}/>)
-
-    return (
-      <div className="todo-list">
-          {todoList}
-      </div>
-    )
-  }
 }
+
+// class App extends React.Component {
+
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if(prevState.todos !== this.state.todos) {
+//       this.setState({
+//         localStorage: localStorage.setItem("local-todo", JSON.stringify(this.state.todos))
+//       })
+//     }
+//   }
+  
+//   render() {
+
+//     const todoList = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleClick={this.handleClick}/>)
+
+//     return (
+//       <div className="todo-list">
+//           {todoList}
+//       </div>
+//     )
+//   }
+// }
 
 export default App
